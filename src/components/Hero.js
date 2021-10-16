da4fc6aaeca9be093b56a020dcb6dd6c7dd56f55
Joustin Ortiz <ortizjoustin@gmail.com>
2021-10-15 21:49:29 -0500

    Detail in hero added and bug in theme fixed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,44 @@
+import React, { useState, useEffect } from "react";
+
 const Hero = ({ lang }) => {
+  const [text, setText] = useState("");
+  const fullText =
+    lang === "en"
+      ? "Hi, I'm Joustin."
+      : null || lang === "es"
+      ? "Hola, soy Joustin."
+      : null;
+
+  useEffect(() => {
+    let num = 0;
+    let textToState = "";
+    let statusText = false;
+
+    setInterval(() => {
+      if (statusText) {
+        textToState = textToState.slice(0, -1);
+        setText(textToState);
+        num -= 1;
+
+        if (textToState === "") {
+          statusText = false;
+        }
+      } else if (!statusText) {
+        textToState = `${textToState}${fullText.charAt(num)}`;
+        num += 1;
+        setText(textToState);
+
+        if (textToState === fullText) {
+          statusText = true;
+        }
+      }
+    }, 300);
+  }, [fullText]);
+
   return (
     <div className="hero min-h-screen">
       <div className="min-h-screen filter-hero flex justify-center items-center">
-        <h2 className="text-white text-3xl font-semibold">
-          {lang === "en" ? "Hi, I'm Joustin" : ""}
-          {lang === "es" ? "Hola, soy Joustin" : ""}.
-        </h2>
+        <h2 className="text-white text-3xl font-semibold">{text}</h2>
       </div>
     </div>
   );
